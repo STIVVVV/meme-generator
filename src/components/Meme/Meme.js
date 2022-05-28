dec219d7690260444ad095d341042060990ac54a
STIVVVV <stevencanfield2000@gmail.com>
2022-05-28 17:14:50 -0400

    I now get a random photo url
    -the photot does not appear

--- src/components/Meme/Meme.js
+++ src/components/Meme/Meme.js
@@ -1,13 +1,22 @@
 import React from "react";
-
+import memesData from "../../memesData";
 
 function Meme() {
+
+    function getMemeImage()
+    {
+        const memeArray = memesData.data.memes;
+        const randomNumber = Math.floor(Math.random() * memeArray.length)
+        const url = memeArray[randomNumber].url;
+        console.log(url);
+    }
+
     return (
       <main>
         <form className="form">
             <input type="text" className="form--input" placeholder="Top text"/>
             <input type="text" className="form--input" placeholder="Bottom Text"/>
-            <button className="form--button">Get a new meme image  🖼</button>
+            <button className="form--button" onClick={getMemeImage}>Get a new meme image 🖼</button>
         </form>
       </main>
     );
