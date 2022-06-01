e8d4bf30fcce4557bf415697ef814ce3fbd6479d
STIVVVV <stevencanfield2000@gmail.com>
2022-06-01 15:10:27 -0400

    Fixed meme image

--- src/components/Meme/Meme.js
+++ src/components/Meme/Meme.js
@@ -1,25 +1,27 @@
 import React from "react";
 import memesData from "../../memesData";
 
-function Meme() {
+function Meme() 
+{
+  const [memeImage, setMemeImage] = React.useState("http://i.imgflip.com/1bij.jpg")
 
-    function getMemeImage()
-    {
-        const memeArray = memesData.data.memes;
-        const randomNumber = Math.floor(Math.random() * memeArray.length)
-        const url = memeArray[randomNumber].url;
-        console.log(url);
-    }
-
-    return (
-      <main>
-        <form className="form">
-            <input type="text" className="form--input" placeholder="Top text"/>
-            <input type="text" className="form--input" placeholder="Bottom Text"/>
-            <button className="form--button" onClick={getMemeImage}>Get a new meme image 🖼</button>
-        </form>
-      </main>
-    );
+  function getMemeImage()
+  {
+    const memeArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memeArray.length)
+    setMemeImage(memeArray[randomNumber].url)
   }
+
+  return (
+    <main>
+      <div className="form">
+          <input type="text" className="form--input" placeholder="Top text"/>
+          <input type="text" className="form--input" placeholder="Bottom Text"/>
+          <button className="form--button" onClick={getMemeImage}>Get a new meme image 🖼</button>
+      </div>
+      <img src={memeImage} className="meme--image" alt="meme-image"/>
+    </main>
+  );
+}
   
-  export default Meme;
+export default Meme;
